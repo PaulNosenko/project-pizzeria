b13792a60d568db53ba77e61a013b84bc730adad
Paul Nosenko <pavelgadzenko@gmail.com>
2023-05-14 17:57:37 +0200

    [main] - Images options added or removed based on options selected

--- src/js/script.js
+++ src/js/script.js
@@ -40,13 +40,13 @@
     },
   };
 
-  const settings = {
-    amountWidget: {
-      defaultValue: 1,
-      defaultMin: 1,
-      defaultMax: 9,
-    }
-  };
+  // const settings = {
+  //   amountWidget: {
+  //     defaultValue: 1,
+  //     defaultMin: 1,
+  //     defaultMax: 9,
+  //   }
+  // };
 
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
@@ -86,6 +86,7 @@
       this.formInputs = this.form.querySelectorAll(select.all.formInputs);
       this.cartButton = this.element.querySelector(select.menuProduct.cartButton);
       this.priceElem = this.element.querySelector(select.menuProduct.priceElem);
+      this.imageWrapper = this.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion() {
@@ -116,36 +117,36 @@
         event.preventDefault();
         this.processOrder();
       });
-      
-      for(let input of this.formInputs){
+
+      for (let input of this.formInputs) {
         input.addEventListener('change', () => {
           this.processOrder();
         });
       }
-      
+
       this.cartButton.addEventListener('click', (event) => {
         event.preventDefault();
         this.processOrder();
       });
     }
-   
+
     processOrder() {
       const thisProduct = this;
-    
+
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-    
+
       // set price to default price
       let price = thisProduct.data.price;
-    
+
       // for every category (param)...
-      for(let paramId in thisProduct.data.params) {
+      for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
         const selectedOptionsIds = formData[paramId];
 
         // for every option in this category
-        for(let currentOptionId in param.options) {
+        for (let currentOptionId in param.options) {
           const option = param.options[currentOptionId];
 
           if (option.default && !selectedOptionsIds.includes(currentOptionId)) {
@@ -155,6 +156,18 @@
           if (!option.default && selectedOptionsIds.includes(currentOptionId)) {
             price += option.price;
           }
+
+          const searchedImageClass = `.${paramId}-${currentOptionId}`;
+          const seachedImage = this.imageWrapper.querySelector(searchedImageClass);
+
+          //check if image for current option exists and display or hide it based on whether it's chosen in the form
+          if (seachedImage) {
+            if (seachedImage && selectedOptionsIds.includes(currentOptionId)) {
+              seachedImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              seachedImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
+          }
         }
       }
       // update calculated price in the HTML
@@ -163,21 +176,21 @@
   }
 
 
-const app = {
-  initMenu: function () {
-    for (let productKey in this.data.products) {
-      new Product(productKey, this.data.products[productKey]);
-    }
-  },
-  initData: function () {
-    this.data = dataSource;
-  },
-  init: function () {
-    this.initData();
-    this.initMenu();
-  },
-};
+  const app = {
+    initMenu: function () {
+      for (let productKey in this.data.products) {
+        new Product(productKey, this.data.products[productKey]);
+      }
+    },
+    initData: function () {
+      this.data = dataSource;
+    },
+    init: function () {
+      this.initData();
+      this.initMenu();
+    },
+  };
 
 
-app.init();
+  app.init();
 }
