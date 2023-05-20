c9dde915e70f8546e36edee5f7e8ee29953a0848
Paul Nosenko <pavelgadzenko@gmail.com>
2023-05-20 17:01:01 +0200

    [8.6] - Implemented remove functionality

--- src/js/script.js
+++ src/js/script.js
@@ -326,6 +326,10 @@
       this.dom.productList.addEventListener('updated', () => {
         this.update();
       });
+
+      this.dom.productList.addEventListener('remove', (event) => {
+        this.remove(event.detail.cartProduct); 
+      });
     }
 
     add(menuProduct) {
@@ -362,6 +366,17 @@
         totalPriceEl.innerHTML = this.totalPrice;
       });
     }
+
+    remove(cartProductToRemove) {
+      //remove from DOM
+      cartProductToRemove.dom.wrapper.remove();
+      
+      //remove from JS array
+      const index = this.products.findIndex((currentProd) => currentProd === cartProductToRemove);
+      this.products.splice(index, 1);
+
+      this.update();
+    }
   }
 
   class CartProduct {
@@ -375,6 +390,7 @@
 
       this.getElements(element);
       this.initAmountWidget();
+      this.initActions();
     }
 
     getElements(element) {
@@ -393,6 +409,28 @@
         this.price = this.priceSingle * this.amountWidget.value;
         this.dom.price.innerHTML = this.price;
       });
+    }
+
+    initActions() {
+      this.dom.edit.addEventListener('click', (event) => {
+        event.preventDefault();
+      });
+
+      this.dom.remove.addEventListener('click', (event) => {
+        event.preventDefault();
+        this.remove();
+      });
+    }
+
+    remove() {
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: this
+        }
+      });
+console.log('removing ...');
+      this.dom.wrapper.dispatchEvent(event);
     }
   }
 
