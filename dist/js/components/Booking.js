4f1f4e0573c7502362500217fa1e23f3a09158b2
Paul Nosenko <pavelgadzenko@gmail.com>
2023-06-14 21:00:23 +0200

    [main] - Added hour and date picker

--- dist/js/components/Booking.js
+++ dist/js/components/Booking.js
@@ -1,5 +1,7 @@
 import { templates, select } from "../settings.js";
 import AmountWidget from "./AmountWidget.js";
+import DatePicker from "./DatePicker.js";
+import HourPicker from "./HourPicker.js";
 
 class Booking {
     constructor(wrapper) {
@@ -12,13 +14,17 @@ class Booking {
         this.dom = {
             wrapper,
             peopleAmount: document.querySelector(select.booking.peopleAmount),
-            hoursAmount: document.querySelector(select.booking.hoursAmount)
+            hoursAmount: document.querySelector(select.booking.hoursAmount),
+            datePicker: document.querySelector(select.widgets.datePicker.wrapper),
+            hourPicker: document.querySelector(select.widgets.hourPicker.wrapper)
         };
     }
 
     initWidgets() {
         this.peopleAmountWidget = new AmountWidget(this.dom.peopleAmount);
         this.hoursAmountWidget = new AmountWidget(this.dom.hoursAmount);
+        this.datePickerWidget = new DatePicker(this.dom.datePicker);
+        this.hourPickerWidget = new HourPicker(this.dom.hourPicker);
 
         this.dom.peopleAmount.addEventListener('updated', () => {});
         this.dom.hoursAmount.addEventListener('updated', () => {});
